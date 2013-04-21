394eab19839683d30592b075b79f4e7eeb4cdca6
Jay Hotta <j-hotta@creationline.com>
2013-04-21 16:59:09 +0900

    rails app base

---
+++ hotmot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotmot::Application.config.secret_token = '3708a09dd591ec1b378ff3ccaaa9c116be1bc04bd62853d8f19cee3ac0a26c70c658fc3e127f6710c609f752754114ebedf54f2440ebbad432580ea8aa157cbd'
